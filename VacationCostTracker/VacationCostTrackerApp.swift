9b78ed670699af7ee6a0b7ea11b3ec2f9bc3d310
Phillip Ruta <phillipt.ruta@gmail.com>
2026-02-20 15:46:19 -0500

    Initial Commit

---
+++ VacationCostTracker/VacationCostTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VacationCostTrackerApp.swift
+//  VacationCostTracker
+//
+//  Created by Phillip Ruta on 2/20/26.
+//
+
+import SwiftUI
+
+@main
+struct VacationCostTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
